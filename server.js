8b8cbdd894ac70b6032e50b7e12a791a9d78d326
Arun Lal <30426100+arunlalkp@users.noreply.github.com>
2017-08-05 21:04:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,71 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne= {
+    title: 'Article One | Arun Lal',
+    heading: 'Article One',
+    date: 'August 5, 2017',
+    content: `<p>Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development!
+                 Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development! Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development! Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development!
+            </p>
+            
+            <p> Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development!
+                 Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development! Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development! Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development!
+            </p>
+             <p> Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development!
+                 Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development! Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development! Hi, This is my first article. on My IMAD console page for just, understand how codeing is works and Learn app development!
+             </p>`
+};
+
+function createTemplate (data) {
+    var title= data.title;
+    var heading= data.heading;
+    var date= data.date;
+    var content= data.content;
+    
+
+
+var htmlTemplate= `
+    
+   <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+    <div class='container'>
+        <div>
+            <a href='/'>Home</a>
+            <hr/>
+        </div>
+          <h3>
+            ${heading}
+          </h3>
+        <div>
+           ${date}  
+        </div>
+        <div>
+           ${content}
+        </div>
+    </div>
+    </body>
+   </html>
+ ` ;
+  return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
