099652a6121c2721d46416787ba9c748308725f5
Arun Lal <30426100+arunlalkp@users.noreply.github.com>
2017-08-06 22:01:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,23 @@ console.log('Loaded!');
 //counter code
 
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function() {
-    
-counter = counter + 1;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
-    
+  //create request object
+    var request = new XMLHttpRequest();
+  //capture the response and store it an variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+            if (request.status === 200) {
+                var counter = response.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+
+//make the request
+    request.open('GET', 'http://arunlalkpkp78.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
