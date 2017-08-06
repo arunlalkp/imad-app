3479bcb1eb8a1b0129e2ab9c6dfd88d34f28726f
Arun Lal <30426100+arunlalkp@users.noreply.github.com>
2017-08-06 17:09:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,11 @@ console.log('Loaded!');
 
 //to change the txt on div
 var element = document.getElementById('main-text');
-
 element.innerHTML = 'Lal Broz';
 
 //move the picture
-
 var img = document.getElementById('arun');
 img.onClick = function () {
-    img.style.marginLeft= '100px';
+    img.style.marginLeft = '100px';
     
 };
