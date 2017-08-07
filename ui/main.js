5777fecbbb4751e6ef0b5cc4b0595c2b1ff8eb90
Arun Lal <30426100+arunlalkp@users.noreply.github.com>
2017-08-07 18:50:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onlick = function () {
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
     for (var i=0; i<names.length; i++) {
-        list +- '<li>' + names[i] + '</li>';
+        list += '<li>' + names[i] + '</li>';
         
     }
     var ui = document.getElementById('namelist');
