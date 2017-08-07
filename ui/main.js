f312a1a73fb508977c686023181373e46f6c8ba9
Arun Lal <30426100+arunlalkp@users.noreply.github.com>
2017-08-07 19:00:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ button.onclick = function() {
 //submit name
 
 var nameInput = document.getElementById('name');
-var names = nameInput.value;
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onlick = function () {
