5a320b75cf2092c27678983939fc6d84127c9600
Arun Lal <30426100+arunlalkp@users.noreply.github.com>
2017-08-12 20:13:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('/counter', function (req, res) {
 
   
 
-app.get('articles/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     //var articleName = req.params.articleName;
     
     pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'" , function(err, result) {
