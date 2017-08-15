db578082c624c41963860c5ca55c66d2b10670f8
Arun Lal <30426100+arunlalkp@users.noreply.github.com>
2017-08-15 22:43:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,16 +65,18 @@ app.get('/', function (req, res) {
 
 //code of password and session
 
-function hash (input, salt) {
-    // how do create a hash ?
-   var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-   return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
-}
+
 
 app.get('/hash/:input', function(req, res){
    var hashedString = hash(req.params.input, 'this-is-some-random-string');
    res.send(hashedString);
 });
+
+function hash (input, salt) {
+    // how do create a hash ?
+   var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+   return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
 
 app.post('/create-user', function(res, req) {
     //username, password
